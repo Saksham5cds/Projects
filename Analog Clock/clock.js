57419d381305024a41ee2f93aac093546d1bf471
Saksham5cds <73445004+Saksham5cds@users.noreply.github.com>
2021-03-15 03:06:33 +0530

    Add files via upload

---
+++ Analog Clock/clock.js
@@ -0,0 +1,16 @@
+const deg = 6;
+const hr = document.querySelector('#hr');
+const mn = document.querySelector('#mn');
+const sc = document.querySelector('#sc');
+
+setInterval(() => {
+  let needle = new Date();
+
+  let hh = needle.getHours() * 30;
+  let mm = needle.getMinutes() * deg;
+  let ss = needle.getSeconds() * deg;
+
+  hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
+  mn.style.transform = `rotateZ(${mm}deg)`;
+  sc.style.transform = `rotateZ(${ss}deg)`;
+})
